83a9e3be5e587ed13560effe8aa25401552198c0
Elias Jr <elijah.junnior@outlook.com>
2022-06-15 08:46:05 -0300

    all requested middlewares created

--- src/index.js
+++ src/index.js
@@ -10,19 +10,107 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  
+  // pega o username dos headers
+  const { username } = request.headers;
+
+  // busca um usuário que corresponda ao username informado
+  const user = users.find(user => user.username === username);
+
+  // verifica que se existe um usuario que atenda ao criterios
+  if(!user) { 
+    // retorna um erro caso o usuario não seja encontrado
+    return response.status(404).json({error: "user does not exist"});
+  }
+
+  // adiciona o usuario a request 
+  request.user = user;
+
+  // avança para a próxima função do fluxo
+  next();
+
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  
+  // busca o usuário colocado por outro middleware na request
+  const { user } = request;
+
+  // valida se o usuario é pro ou ainda não atingiu o limite de todos free
+  if(user.todos.length >= 10 && !user.pro) { 
+    return response.status(403).json({
+      error: "the maximum limit of ten tasks in free plan has been reached"
+    });
+  }
+
+  // avança para a próxima função do fluxo
+  next();
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  
+  // pega os parametros passados pelo request
+  const { username } = request.headers;
+  const { id } = request.params;
+  
+  // busca um usuário que possua o username informado
+  const user = users.find(user => user.username === username);
+  
+  // valida se o usuário existe
+  if(!user) { 
+    return response.status(404).json({
+      error: "user does not exist"
+    });
+  }
+  
+  // valida se o id informado é um uuid
+  if(!validate(id)) { 
+    return response.status(400).json({
+      error: "invalid todo id"
+    });
+  }
+
+  // busca um todo com o id informado
+  const todo = user.todos.find(todo => todo.id === id); 
+
+  // valida se o todo existe
+  if(!todo) { 
+    return response.status(404).json({
+      error: "erro"
+    });
+  }
+
+  // grava o usuario e o todo encontrados na requisição
+  request.user = user;
+  request.todo = todo;
+
+  // avança para a próxima funçõa do fluxo
+  next();
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  
+  // pega os parametros passados pela requisição
+  const { id } = request.params;
+
+  // busca um usuário com o id informado
+  const user = users.find(user => user.id === id);
+
+  // valida se o usuário existe
+  if(!user) { 
+    return response.status(404).json({ 
+      error: "user does not exist"
+    });
+  }
+
+  // passa o usuário encontrado para a requisição
+  request.user = user; 
+
+  // avança para a proximo função do fluxo
+  next();
+
 }
 
 app.post('/users', (request, response) => {
